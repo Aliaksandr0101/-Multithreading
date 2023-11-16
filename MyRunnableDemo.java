8ddd7a8703931d473ffbd862bb5a25221952ef78
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-11-17 00:27:39 +0300

    FirstCommit

---
+++ MyRunnableDemo.java
@@ -0,0 +1,21 @@
+
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Важная работа выполняется.");
+    }
+}
+public class MyRunnableDemo {
+
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+
+        Thread thread1 = new Thread(myRunnable);
+        Thread thread2 = new Thread(myRunnable);
+        Thread thread3 = new Thread(myRunnable);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
