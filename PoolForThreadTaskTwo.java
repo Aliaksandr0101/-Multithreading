4479ca33734e284578aa352c484d2697856ef3c6
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-05-18 18:06:37 +0300

    CommitPoolExecutor

---
+++ PoolForThreadTaskTwo.java
@@ -0,0 +1,54 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PoolForThreadTaskTwo {
+    public static void main(String[] args) {
+
+        ProcessThread processThreadOne = new ProcessThread("Thread 1");
+        ProcessThread processThreadTwo = new ProcessThread("Thread 2");
+        ProcessThread processThreadThree = new ProcessThread("Thread 3");
+        ProcessThread processThreadFour = new ProcessThread("Thread 4");
+        ProcessThread processThreadFive = new ProcessThread("Thread 5");
+        ProcessThread processThreadSix = new ProcessThread("Thread 6");
+        ProcessThread processThreadSeven = new ProcessThread("Thread 7");
+        ProcessThread processThreadEight = new ProcessThread("Thread 8");
+        ProcessThread processThreadNine = new ProcessThread("Thread 9");
+        ProcessThread processThreadTen = new ProcessThread("Thread 10");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        System.out.println("Start of threads");
+
+        executorService.submit(processThreadOne);
+        executorService.submit(processThreadTwo);
+        executorService.submit(processThreadThree);
+        executorService.submit(processThreadFour);
+        executorService.submit(processThreadFive);
+        executorService.submit(processThreadSix);
+        executorService.submit(processThreadSeven);
+        executorService.submit(processThreadEight);
+        executorService.submit(processThreadNine);
+        executorService.submit(processThreadTen);
+
+
+        executorService.shutdown();
+        System.out.println("The end of threads");
+    }
+
+}
+
+class ProcessThread extends Thread{
+
+    String nameThread;
+
+    public ProcessThread(String nameThread) {
+        this.nameThread = nameThread;
+    }
+
+    @Override
+    public void run() {
+        for (int i =0; i < 10; i++) {
+            System.out.println("The name - " + nameThread + " : " + i);
+        }
+    }
+}
--- PoolForThreeThread.java
+++ PoolForThreeThread.java
@@ -6,27 +6,25 @@ public class PoolForThreeThread {
     public static void main(String[] args) {
 
         NameThread nameThreadOne = new NameThread("Thread 1");
-        NameThread nameThreadTwo = new NameThread("Thread 2");
-        NameThread nameThreadThree = new NameThread("Thread 3");
+        NameThread processThreadTwo = new NameThread("Thread 2");
+        NameThread processThreadThree = new NameThread("Thread 3");
 
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-        
+
         executorService.submit(nameThreadOne);
-        executorService.submit(nameThreadTwo);
-        executorService.submit(nameThreadThree);
+        executorService.submit(processThreadTwo);
+        executorService.submit(processThreadThree);
 
 
         executorService.shutdown();
-
-
+        System.out.println("finish");
     }
 
 }
 class NameThread extends Thread{
 
     String nameThread;
-
 
     public NameThread(String nameThread) {
         this.nameThread = nameThread;
@@ -35,7 +33,7 @@ class NameThread extends Thread{
     @Override
     public void run() {
         for (int i = 1; i < 11; i++) {
-            System.out.println("The name - " + nameThread + " : output " + i);
+            System.out.println("The name - " + nameThread);
         }
     }
 }
