aa8b2d7a77597484452b49412580ab3ab6c6eae9
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-05-23 07:51:38 +0300

    CommitSynchronizedCollection

--- ExampleWithSynchronizedCollection.java
+++ ExampleWithSynchronizedCollection.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ExampleWithSynchronizedCollection {
-    private int n = 100; 
     public static void main(String[] args) {
-        
-     List<Integer> synchronizedListWithNumbers = Collections.synchronizedList(new ArrayList<>());
-        for (int i = 0; i < ; i++) {
-            
-        }
+        int n = 100;
 
-     
+     List<Integer> synchronizedListWithNumbers = Collections.synchronizedList(new ArrayList<>());
+        synchronizedListWithNumbers.addAll(IntStream.range(0,n)
+                                           .boxed()
+                                            .collect(Collectors.toList()));
+
+     synchronizedListWithNumbers.forEach(System.out::println);
     }
 }
