6ff7fed0f26816db017a1c451ffccb064dea252a
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-05-23 09:29:36 +0300

    CommitSynchronizedCollection

--- ExampleWithSynchronizedCollection.java
+++ ExampleWithSynchronizedCollection.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,7 +15,28 @@ public class ExampleWithSynchronizedCollection {
         synchronizedListWithNumbers.addAll(IntStream.range(0,n)
                                            .boxed()
                                             .collect(Collectors.toList()));
+        System.out.println("List before summation\n");
+        synchronizedListWithNumbers.forEach(System.out::println);
 
-     synchronizedListWithNumbers.forEach(System.out::println);
+        System.out.println("\nList after summation\n");
+
+     ExecutorService poolThreeStreams = Executors.newFixedThreadPool(3);
+
+        try {
+            List<Integer> incrementedNumbers = poolThreeStreams.submit(() ->
+                    synchronizedListWithNumbers.parallelStream()
+                            .map(num -> num + 10)
+                            .collect(Collectors.toList())
+            ).get();
+            poolThreeStreams.shutdown();
+            incrementedNumbers.forEach(System.out::println);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
