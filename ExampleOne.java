2f20179047e5c2591316da42917b1d3b32ab7321
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-05-29 22:24:38 +0300

    CommitExample94

---
+++ ExampleOne.java
@@ -0,0 +1,6 @@
+public class ExampleOne {
+    public static void main(String[] args) {
+        int numbersOf = Runtime.getRuntime().availableProcessors();
+        System.out.println(numbersOf);
+    }
+}
---
+++ NeuThread.java
@@ -0,0 +1,20 @@
+import java.util.stream.IntStream;
+
+public class NeuThread  extends Thread{
+    private String symbol;
+
+    public NeuThread(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public void run() {
+        IntStream.range(0,100).forEach(i -> System.out.println(i + " " + symbol));
+    }
+
+    public static void main(String[] args) {
+        NeuThread neuThread = new NeuThread("*G");
+        neuThread.start();
+        
+    }
+}
