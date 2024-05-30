d36b25805e03eab5bc1bf8b9fd086fadabd151ff
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-05-30 22:25:25 +0300

    CommitLibrary

--- ExampleOne.java
+++
@@ -1,6 +0,0 @@
-public class ExampleOne {
-    public static void main(String[] args) {
-        int numbersOf = Runtime.getRuntime().availableProcessors();
-        System.out.println(numbersOf);
-    }
-}
