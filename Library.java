05c6314337b373490a47b8b17d733ea3e3bf868c
Aliaksandr Komar <Komarthebest1992@gmail.com>
2024-05-31 23:04:45 +0300

    CommitLibrary

--- Library.java
+++ Library.java
@@ -1,48 +1,56 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Library {
     public static void main(String[] args) {
+        ExecutorService numbersOfThread = Executors.newFixedThreadPool(7);
         Semaphore placeInTheLibrary = new Semaphore(7);
         Semaphore numbersBooks = new Semaphore(100);
-        int numberOfPerson = 10;
+        int numberOfPerson = 60;
         for (int i = 0; i < numberOfPerson; i++) {
-            TakeThePlace personTakeThePlace = new TakeThePlace();
+            PersonInLibrary personTakeThePlace = new PersonInLibrary("Visitor №" + (i+1));
             personTakeThePlace.placeInTheLibrary = placeInTheLibrary;
             personTakeThePlace.numbersBooks = numbersBooks;
-            personTakeThePlace.start();
+            numbersOfThread.submit(personTakeThePlace);
         }
+        numbersOfThread.shutdown();
+    }
+}
 
+class PersonInLibrary extends Thread{
+    String numberVisitor;
 
+    public PersonInLibrary(String numberVisitor) {
+        this.numberVisitor = numberVisitor;
     }
 
-}
-class TakeThePlace extends Thread{
     Semaphore placeInTheLibrary;
-
 
     @Override
     public void run() {
 
-        synchronized (placeInTheLibrary) {
             try {
                 placeInTheLibrary.acquire();
-                System.out.println("Занял место " + Thread.currentThread().getName() + "осталось мест " + placeInTheLibrary.availablePermits());
+                System.out.println("Занял место " + numberVisitor + ".");
                 TakeBooks();
                 Thread.sleep(2000);
                 placeInTheLibrary.release();
-                System.out.println("Освободил место " + Thread.currentThread().getName() + "осталось мест " + placeInTheLibrary.availablePermits());
+                System.out.println("Освободил место " + numberVisitor + ".");
+
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-    }
+
     Semaphore numbersBooks;
-    Lock lock = new ReentrantLock();
+    Lock lockOneLibrarian = new ReentrantLock();
 
     public void TakeBooks() {
-        lock.lock();
+        lockOneLibrarian.lock();
         for (int i = 0; i < 3; i++){
             try {
                 numbersBooks.acquire();
@@ -50,44 +58,18 @@ class TakeThePlace extends Thread{
                 e.printStackTrace();
             }
         }
-        System.out.println(Thread.currentThread().getName() + "взял три книги" + "осталось" + numbersBooks.availablePermits());
-        lock.unlock();
-        System.out.println("читает");
+        System.out.println(numberVisitor + " взял три книги.");
+        System.out.println(numberVisitor + " начал читать.");
+        lockOneLibrarian.unlock();
         try {
             Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("вернул книги");
-        numbersBooks.release();
-
-    }
-}
-/*
-class PersonL{
-    Semaphore numbersBooks;
-    Lock lock = new ReentrantLock();
-
-    public void TakeBooks() {
-        lock.lock();
-        for (int i = 0; i < 3; i++){
-            try {
-                numbersBooks.acquire();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            System.out.println(numberVisitor + " закончил чтение.");
+            for (int i = 0; i < 3; i++) {
+                numbersBooks.release();
             }
-        }
-        System.out.println(Thread.currentThread().getName() + "взял три книги" + "осталось" + numbersBooks.availablePermits());
-        lock.unlock();
-        System.out.println("читает");
-        try {
-            Thread.sleep(2000);
+            System.out.println(numberVisitor + " вернул книги.");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("вернул книги");
-        numbersBooks.release();
-
     }
 }
-*/
